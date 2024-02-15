8730f401345dc9ec131d431b3c96bdea57b93455
Mayur Yaduvanshi <singhmayurraj97@gmail.com>
2024-02-15 23:58:17 +0530

    adding morgan to log  HTTP requests

--- app.mjs
+++ app.mjs
@@ -1,8 +1,10 @@
 // const express = require('express')
 import express from 'express';
 import productRoutes from "./api/routes/products.mjs"
-import orderRoutes from "./api/routes/products.mjs"
+import orderRoutes from "./api/routes/orders.mjs"
+import morgan from 'morgan';
 const app = express();
+app.use(morgan('dev'))
 
 app.use('/orders' , orderRoutes)
 app.use('/products' , productRoutes)
