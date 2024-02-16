7103fc7b85fd4e92f1b06473202aa21dcb105d5c
Mayur Yaduvanshi <singhmayurraj97@gmail.com>
2024-02-16 23:58:40 +0530

    created routes for orders endpoint

--- api/routes/orders.mjs
+++ api/routes/orders.mjs
@@ -3,17 +3,34 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req ,res,next)=>{
-    res.status(200).json();
-})
-router.get('/:orderStatus', (req ,res,next)=>{
     res.status(200).json({
-        orderStatus : 'orderPlaced'
-    })
+        message : "welcome to orders section using GET request"
+    });
+})
+router.get('/:StatusID', (req ,res,next)=>{
+    const id = req.params.StatusID;
+    if(id==='uniID'){
+        res.status(200).json({
+            message : "Hey mayur orders here",
+            ID : id
+        })
+    }
+    else{
+        res.status(200).json({
+            message : "provided invalid order id"
+        })
+    }
 })
 
+
 router.post('/', (req ,res,next)=>{
+    const order = {
+        productID : req.body.productID,
+        quantity : req.body.quantity
+    }
     res.status(200).json({
-        orderStatus : 'POST Method'
+        orderStatus : 'POST Method',
+        order : order
     })
 })
 
