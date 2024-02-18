ed63d851ac2fdaa6bf1656e73970cd2f89c50261
Mayur Yaduvanshi <singhmayurraj97@gmail.com>
2024-02-18 13:31:37 +0530

    worked with mongoose to manage database

--- api/routes/products.mjs
+++ api/routes/products.mjs
@@ -5,55 +5,104 @@ import Product from "../models/product.mjs";
 const router = express.Router();
 
 router.get('/' , (req,res,next)=>{
-    res.status(200).json({
-        message : "Handling the GET reqs to the products"
+   Product.find()
+   .exec()
+   .then(docs =>{
+        console.log(docs);
+        res.status(200).json(docs);
+   })
+   .catch(err =>{
+    console.log(err);
+    res.status(500).json({
+        error : err
     })
+   })
 })
 
 router.post('/' , (req,res,next)=>{
-    // const product = {
-    //         name : req.body.name,
-    //         price : req.body.name
-    //     }
+  
     const product = new Product({
         _id  : new mongoose.Types.ObjectId(),
         name : req.body.name,
-        price : req.body.name
+        price : req.body.price
     })
     product.save().then(result =>{
         console.log(result);
-    }).catch(err => console.log(err));
+        res.status(200).json({
+            message : "Handling the POST reqs to the products",
+            CreatedProduct : product
+        })
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error : err
+        })
+    });
 
-    res.status(200).json({
-        message : "Handling the POST reqs to the products",
-        CreatedProduct : product
-    })
+    
 })
 router.get('/:productID' , (req,res,next)=>{
     const id = req.params.productID;
-    if(id==='special'){
-        res.status(200).json({
-            message : "Personal Discount",
-            ID : id
+    Product.findById(id)
+    .exec()
+    .then(doc => {
+        console.log("From database" + doc);
+        if(doc){
+        res.status(200).json(doc)
+        }else{
+            res.status(404).json({
+                message: "No valid entry found for provided id"
+            })
+        }
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({
+            message:"The ID provided is not correct , please recheck"
+
         })
-    }
-    else{
-        res.status(200).json({
-            message : "provided invalid id"
-        })
-    }
+    })
     
 })
 
-router.patch('/' , (req,res,next)=>{
-    res.status(200).json({
-        message : "Handling the PATCH reqs to the products"
+router.patch('/:productID' , (req,res,next)=>{
+    const id = req.params.productID;
+    const updateOps = {};
+    for(const ops of req.body){
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.updateOne({
+        _id : id
+    },{
+        $set : updateOps
+    })
+    .exec()
+    .then(result =>{
+        console.log(result)
+        res.status(200).json(result);
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({
+            error : err
+        })
     })
 })
 
-router.delete('/' , (req,res,next)=>{
-    res.status(200).json({
-        message : "Handling the DELETE reqs to the products"
+router.delete('/:productID' , (req,res,next)=>{
+    const id = req.params.productID;
+    Product.deleteOne({
+        _id : id
+    })
+    .exec()
+    .then(result =>{
+        res.status(200).json(result);
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({
+            error : err
+        })
     })
 })
 
